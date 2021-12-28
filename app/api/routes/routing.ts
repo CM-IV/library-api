5f2f1de63a0deeab71e8d3b002618ddccea6ce10
CM-IV <cmathIV@protonmail.com>
2021-12-27 22:08:32 -0600

    Added healthcheck for API and procfile for heroku deployment

--- app/api/routes/routing.ts
+++ app/api/routes/routing.ts
@@ -7,6 +7,7 @@
 |
 */
 
+import HealthCheck from "@ioc:Adonis/Core/HealthCheck";
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
@@ -35,6 +36,15 @@ Route.group(() => {
     //logout
     Route.post("/logout", "AuthController.logout");
   }).middleware("auth");
+
+  //Health check
+  Route.get("health", async ({ response }) => {
+
+    const report = await HealthCheck.getReport();
+
+    return report.healthy ? response.ok(report) : response.badRequest(report);
+
+  })
 
   //get /books list
   //get /books/:id
